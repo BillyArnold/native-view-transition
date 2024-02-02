48fb233238c2cd3c0b30e3d442bef5d9b5528d97
Billy Arnold <billyarnoldd@gmail.com>
2024-02-02 18:46:24 +0000

    Setting up repo with nativewind

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, SafeAreaView, ScrollView, Image } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="flex-1">
+      <ScrollView className="py-6 px-2 h-full">
+        <View
+          className="
+          w-full 
+          bg-gray-500 
+          rounded-xl 
+          shadow-black 
+          shadow-2xl
+          flex
+          flex-col
+          min-h-0
+          overflow-hidden
+        "
+        >
+          <Image
+            source={{
+              uri: "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg",
+            }}
+            className="w-full h-40"
+          />
+          <View className="px-4 py-8 min-h-0">
+            <Text className="font-bold">Heading</Text>
+            <Text className="">description</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
